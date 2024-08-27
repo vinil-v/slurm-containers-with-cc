a583274a686b767d1d19013c477cd5dbf2bd2939
Vinil Vadakkepurakkal <82436391+vinil-v@users.noreply.github.com>
2024-08-27 14:29:25 +0530

    Update pyxis-install.sh

--- pyxis-install.sh
+++ pyxis-install.sh
@@ -5,7 +5,7 @@ cd pyxis
 make install
 # Add pyxis plug-in path to 'plugstack.conf’, which is in the same directory as ‘slurm.conf’ by default.
 # Ref: https://slurm.schedmd.com/spank.html
-cat << END >> /etc/slurm/plugstack.conf
+cat < END > /etc/slurm/plugstack.conf
 required /usr/local/lib/slurm/spank_pyxis.so
 END
 systemctl restart slurmd
